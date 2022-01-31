38fa60c5716f7ae2d793ae4f05fa2ee395b61549
Mauro Gutierrez <yoguiimaster@gmail.com>
2022-01-31 12:00:07 -0300

    se termino el reto

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./task-basic.css";
+import TaskBasic from "./task-basic";
+
+ReactDOM.render(<TaskBasic />, document.getElementById("root"));
